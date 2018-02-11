379f7ec7ff0c410d155f70621c93548b7b3c46a4
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-02-10 21:33:19 -0500

    finished project

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -34,28 +34,79 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    // TODO Step 5: Initialise instance variables here
-    
-    
-    
+    var chapter: Int = 0;
+    var stories: Array = [String]()
+    var answers: [[Int:String]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
+        stories = [story1, story2, story3, story4, story5, story6]
+        answers = [
+            [1:answer1a, 2:answer1b],
+            [1:answer2a,2:answer2b],
+            [1:answer3a,2:answer3b]
+        ]
+        nextChapter(tag: 0)
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
+        if sender.titleLabel!.text == "Start Over" {
+            startOver()
+            return
+        }
+        nextChapter(tag: Int(sender.tag))
         
+    }
     
+    func nextChapter(tag: Int){
+        if chapter == 0 {
+            storyTextView.text = stories[chapter]
+            topButton.setTitle(answers[chapter][1], for: .normal)
+            bottomButton.setTitle(answers[chapter][2], for: .normal)
+            chapter = chapter + 1
+        } else if chapter == 1 {
+            //hopped in
+            if tag == 1 {
+                chapter = chapter + 1
+                storyTextView.text = stories[2]
+            //asked if murderer
+            } else if tag == 2 {
+                storyTextView.text = stories[1]
+            }
+            topButton.setTitle(answers[chapter][1], for: .normal)
+            bottomButton.setTitle(answers[chapter][2], for: .normal)
+            chapter = chapter + 1
+        } else if chapter == 2 {
+            //you decided to change the tire yourself
+            if tag == 2 {
+                //copped out
+                storyTextView.text = stories[3]
+                endStory()
+            } else if tag == 1 {
+                //you've gotten in the car
+                storyTextView.text = stories[2]
+                topButton.setTitle(answers[chapter][1], for: .normal)
+                bottomButton.setTitle(answers[chapter][2], for: .normal)
+                chapter = chapter + 1
+            }
+        } else if chapter == 3 {
+            //1. love elton john. 2. stab driver
+            storyTextView.text = (tag == 1) ? stories[5] : stories[4]
+            endStory()
+        }
+    }
+    
+    func endStory(){
+        topButton.isHidden = true
+        bottomButton.setTitle("Start Over", for: .normal)
+    }
+    
+    func startOver(){
+        topButton.isHidden = false;
+        chapter = 0;
+        nextChapter(tag: 0)
     }
     
 
